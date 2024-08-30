ee0f415765a6f42186ae74aa89b860286e2d56c4
Deven Kapadia <devenkapadia1@gmail.com>
2024-08-30 12:20:47 +0530

    till 20

---
+++ p20.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    while (1) {
+        sleep(1); 
+    }
+    return 0;
+}
+
+
+// ps -o pid,comm,nice -p 6313
+
+
+//     PID COMMAND          NI
+//    6313 p20               0
